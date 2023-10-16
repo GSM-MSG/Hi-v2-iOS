3db50dd211225979d764efff0f9256d7fe56abaf
uuuunseo <jdbstjoo@gmail.com>
2023-10-17 08:51:49 +0900

    :sparkles: :: UserDomain Module

--- Plugin/DependencyPlugin/ProjectDescriptionHelpers/ModulePaths.swift
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/ModulePaths.swift
@@ -30,6 +30,7 @@ public extension ModulePaths {
 
 public extension ModulePaths {
     enum Domain: String, MicroTargetPathConvertable {
+        case UserDomain
         case AuthDomain
         case BaseDomain
     }
